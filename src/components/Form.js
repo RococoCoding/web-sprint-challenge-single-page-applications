61c878b10a8ff7c2fba58a87e5f15072ce0a4869
Alice Chang <sinsofangels@gmail.com>
2020-10-16 11:54:24 -0400

    Add yup verification and axios post

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
 import styled from "styled-components";
 import axios from "axios";
+import * as yup from "yup"
 //name, pizza size dropdown, checklist toppings, instructions textarea/ add order
-const FormContainer = styled.div`
+const FormContainer = styled.form`
   
 `
 const Title = styled.div`
@@ -25,8 +26,21 @@ const Select = styled.select`
 `
 
 const Error = styled.p`
-
+  color: red;
  ` 
+
+const Button = styled.button`
+
+`
+
+const formSchema = yup.object().shape({
+  name: yup 
+    .string()
+    .min(2, "Your name must be at least two characters long."),
+  size: yup
+    .string()
+    .min(1, "You must pick a size"),
+});
 
 export default function Form(props) {
   const [formState, setFormState] = useState({
@@ -38,27 +52,26 @@ export default function Form(props) {
 
   const [errorsState, setErrorsState] = useState({});
 
-  
-  // function submit(e) {
-  //   e.preventDefault();
-  //   formSchema.validate(formState, {abortEarly: false})
-  //     .then(value=>setErrorsState({}))
-  //     .catch(err=> {
-  //       let errors = err.inner;
-  //       let errorsObj = {};
-  //       for (let i in errors) {
-  //         let key = errors[i].params.path;
-  //         errorsObj[key] = errors[i].errors[0];
-  //       }
-  //       setErrorsState(errorsObj);
-  //   });
-  //   formSchema.isValid(formState)
-  //     .then(valid=> {
-  //       axios.post("https://reqres.in/api/users", formState) //submit form data to url
-  //       .then(res => console.log(res))
-  //       .catch(err => console.log(err));
-  //   })
-  // }
+  function submit(e) {
+    e.preventDefault();
+    formSchema.validate(formState, {abortEarly: false})
+      .then(value=>setErrorsState({}))
+      .catch(err=> {
+        let errors = err.inner;
+        let errorsObj = {};
+        for (let i in errors) {
+          let key = errors[i].params.path;
+          errorsObj[key] = errors[i].errors[0];
+        }
+        setErrorsState(errorsObj);
+    });
+    formSchema.isValid(formState)
+      .then(valid=> {
+        axios.post("https://reqres.in/api/users", formState) //submit form data to url
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    })
+  }
 
   function updateForm(e) {
     if (e.target.name === "toppings") {
@@ -70,92 +83,96 @@ export default function Form(props) {
   }
 
   return (
-    <FormContainer>
+    <div className="form-page-container">
       <Title>Build Your Own Pizza</Title>
-      <InputContainer>
-        <Label htmlFor="name">Name:</Label>
-        <Input
-          type="text"
-          name="name"
-          placeholder="Mr. Peps"
-          onChange={updateForm}
-        />
-        {errorsState.name && <Error>{errorsState.name}</Error>}
-      </InputContainer>
+      <FormContainer onSubmit={submit}>
+        <InputContainer>
+          <Label htmlFor="name">Name:</Label>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Mr. Peps"
+            onChange={updateForm}
+          />
+          {errorsState.name && <Error>{errorsState.name}</Error>}
+        </InputContainer>
 
 
-      <InputContainer>
-        <Select
-          type="dropdown"
-          name="size"
-          onChange={updateForm}
-        >
-          <option value="">Pick a size</option>
-          <option value="small">Small</option>
-          <option value="medium">Medium</option>
-          <option value="large">Large</option>
-          <option value="xl">XL</option>
-        </Select>
-        {errorsState.size && <Error>{errorsState.size}</Error>}
-      </InputContainer>
+        <InputContainer>
+          <Select
+            type="dropdown"
+            name="size"
+            onChange={updateForm}
+          >
+            <option value="">Pick a size</option>
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+            <option value="xl">XL</option>
+          </Select>
+          {errorsState.size && <Error>{errorsState.size}</Error>}
+        </InputContainer>
 
 
-      <InputContainer>
-        <p>Select toppings:</p>
-        <Label htmlFor="toppings">
-          <Input
-            type="checkbox"
-            name="toppings"
-            value="pepperoni"
-            onChange={updateForm}
-          />pepperoni
-        </Label>
-        <Label htmlFor="toppings">
-          <Input
-            type="checkbox"
-            name="toppings"
-            value="pineapple"
-            onChange={updateForm}
-          />pineapple
-        </Label>
-        <Label htmlFor="toppings">
-          <Input
-            type="checkbox"
-            name="toppings"
-            value="bell peppers"
-            onChange={updateForm}
-          />bell peppers
-        </Label>
-        <Label htmlFor="toppings">
-          <Input
-            type="checkbox"
-            name="toppings"
-            value="mushrooms"
-            onChange={updateForm}
-          />mushrooms
-        </Label>
-        <Label htmlFor="toppings">
-          <Input
-            type="checkbox"
-            name="toppings"
-            value="peanut butter"
-            onChange={updateForm}
-          />peanut butter
-        </Label>
-        {errorsState.toppings && <Error>{errorsState.toppings}</Error>}
-      </InputContainer>
-  
+        <InputContainer>
+          <p>Select toppings:</p>
+          <Label htmlFor="toppings">
+            <Input
+              type="checkbox"
+              name="toppings"
+              value="pepperoni"
+              onChange={updateForm}
+            />pepperoni
+          </Label>
+          <Label htmlFor="toppings">
+            <Input
+              type="checkbox"
+              name="toppings"
+              value="pineapple"
+              onChange={updateForm}
+            />pineapple
+          </Label>
+          <Label htmlFor="toppings">
+            <Input
+              type="checkbox"
+              name="toppings"
+              value="bell peppers"
+              onChange={updateForm}
+            />bell peppers
+          </Label>
+          <Label htmlFor="toppings">
+            <Input
+              type="checkbox"
+              name="toppings"
+              value="mushrooms"
+              onChange={updateForm}
+            />mushrooms
+          </Label>
+          <Label htmlFor="toppings">
+            <Input
+              type="checkbox"
+              name="toppings"
+              value="peanut butter"
+              onChange={updateForm}
+            />peanut butter
+          </Label>
+          {errorsState.toppings && <Error>{errorsState.toppings}</Error>}
+        </InputContainer>
+    
 
-      <InputContainer>
-        <Label htmlFor="instructions">Special Instructions:</Label>
-        <textarea
-          name="instructions"
-          value={formState.instructions}
-          placeholder="Please whisper sweet nothings to my pizza."
-          onChange={updateForm}
-        />
-        {errorsState.instructions && <Error>{errorsState.instructions}</Error>}
-      </InputContainer>
-    </FormContainer>
+        <InputContainer>
+          <Label htmlFor="instructions">Special Instructions:</Label>
+          <textarea
+            name="instructions"
+            value={formState.instructions}
+            placeholder="Please whisper sweet nothings to my pizza."
+            onChange={updateForm}
+          />
+          {errorsState.instructions && <Error>{errorsState.instructions}</Error>}
+        </InputContainer>
+
+        <Button type="submit">Place Your Order</Button>
+      </FormContainer>
+    </div>
   )
 }
